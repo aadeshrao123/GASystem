f780615ff1e65a1c858391f9d2495a52e1e6da09
Aadesh Yadav <55663044+aadeshrao123@users.noreply.github.com>
2023-03-16 19:04:13 +0530

    Advanced physical material
    Created New CPP and header file for a new Physical Material and added a new UPROPERTY and a variable for FootStepSound on that material and added new physical surface in Project Settings

---
+++ Source/GASystem/PhysicalMaterial/GAS_PhysicalMaterial.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
+#include "GAS_PhysicalMaterial.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASYSTEM_API UGAS_PhysicalMaterial : public UPhysicalMaterial
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PhysicalMaterial")
+	class USoundBase* FootStepSound = nullptr;
+	
+};
